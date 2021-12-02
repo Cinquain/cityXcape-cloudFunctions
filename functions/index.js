5525f3c40d6b7bd29e31a76f0fab46ff4c925385
James Allan <James@cityXcape.com>
2021-12-02 08:03:29 -0700

    added verification cloud function

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,81 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
 
+
+exports.checkedInSpot = functions.firestore
+      .document('posts/{postId}/verifiers/{userId}')
+      .onWrite(async (change, context) => {
+
+        let verifierId = context.params.userId;
+        let postId = context.params.postId;
+        let postOwnerId = ''
+        
+
+        console.log(verifierId, postId, postOwnerId);
+
+        var db = admin.firestore();
+
+        return db.collection('posts').doc(postId)
+                  .get()
+                  .then(snapshot => {
+                    
+                    let data = snapshot.data();
+                    postOwnerId = data.owner_id;
+                    let spotName = data.spot_name;
+
+                return db.collection('users').doc(postOwnerId)
+                        .get()
+                        .then(snapshot => {
+                          let ownerData = snapshot.data();
+                          let fcmToken = ownerData.fcmToken;
+
+
+                          return db.collection('users').doc(verifierId)
+                                    .get()
+                                    .then(snapshot => {
+                                      let verifierData = snapshot.data()
+                                      let username = verifierData.displayName
+                                      let imageUrl = verifierData.profileImageUrl
+                                      let reputation = String(verifierData.streetCred) 
+                                      let bio = verifierData.bio
+
+                                      var payload = {
+                                      notification: {
+                                        title: "You've earned 1 StreetCred",
+                                        body: username + ' has verified ' + spotName
+                                      },
+                                      data: {
+                                        userid: verifierId,
+                                        profileUrl: imageUrl,
+                                        userDisplayName: username,
+                                        streetCred: reputation,
+                                        biography: bio
+                                      }
+                                    }
+
+                                    admin.messaging().sendToDevice(fcmToken, payload)
+                                          .then(response => {
+                                            console.log('Successfully sent push notifications', response)
+                                          })
+                                          .catch(error => {
+                                            console.log('Failed to send push notifications', error)
+                                          })
+
+                                    })
+                                    .catch(error => {
+                                      console.log('Error sending push notification', error)
+                                    })
+                        })
+                        .catch(error => {
+                          console.log('Error accessing verifiers branch', error)
+                        })
+                          })
+
+                  .catch(error => {
+                    console.log('Error accessing post branch', error)
+                  })
+      })
+
 exports.savedSecretSpot = functions.firestore
       .document('posts/{postId}/savedBy/{userId}')
       .onWrite(async (change, context) => {
